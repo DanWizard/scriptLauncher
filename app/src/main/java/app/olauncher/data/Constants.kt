969989b5fcebfe8fc3525a2ce420f1c3e57c0f37
zam <zam@pocket>
2024-10-14 21:47:25 -0400

    change flag conflict

--- app/src/main/java/app/olauncher/data/Constants.kt
+++ app/src/main/java/app/olauncher/data/Constants.kt
@@ -81,10 +81,10 @@ object Constants {
     const val FLAG_SET_HOME_APP_11 = 11
     const val FLAG_SET_HOME_APP_12 = 12
 
-    const val FLAG_SET_SWIPE_LEFT_APP = 11
-    const val FLAG_SET_SWIPE_RIGHT_APP = 12
     const val FLAG_SET_CLOCK_APP = 13
     const val FLAG_SET_CALENDAR_APP = 14
+    const val FLAG_SET_SWIPE_LEFT_APP = 16
+    const val FLAG_SET_SWIPE_RIGHT_APP = 17
 
     const val REQUEST_CODE_ENABLE_ADMIN = 666
     const val REQUEST_CODE_LAUNCHER_SELECTOR = 678
